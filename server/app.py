c0d33a130a9821c3f8d2c69e69b2fa79f065cb63
matthew rendely <matthew.rendely@gmail.com>
2023-09-26 23:07:42 -0700

    Setup Procfile.dev for honcho to run full stack

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
+import os 
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Woof!'
+    DATABASE_URI = os.environ.get('DATABASE_URI')
+    return DATABASE_URI
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
