93379fb50506b6da20630d049be6e0007a556378
matthew rendely <matthew.rendely@gmail.com>
2023-10-14 18:04:29 -0700

    Add User, Podcast and follows models

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './SearchBar.css';
 
 
-
 export default function SearchBar({ setResults }) {
   const [keyword, setKeyword] = useState('');
 
--- server/models.py
+++ server/models.py
@@ -1,6 +1,15 @@
 from config import db
 from sqlalchemy_serializer import SerializerMixin
 
+
+follows = db.Table(
+  'follows',
+  db.Model.metadata,
+  db.Column('user_id', db.ForeignKey('users.id'), primary_key=True),
+  db.Column('podcast_id', db.ForeignKey('podcasts.id'), primary_key=True),
+  extend_existing=True
+)
+
 class User(db.Model, SerializerMixin):
 
   __tablename__ = 'users'
@@ -9,20 +18,21 @@ class User(db.Model, SerializerMixin):
   id = db.Column(db.Integer, primary_key = True)
   name = db.Column(db.String, nullable=False)
   email = db.Column(db.String, nullable=False)
-  follows = db.relationship('Follow', back_populates='user')
+  followed_podcasts = db.relationship('Podcast', secondary=follows)
 
   def __repr__(self):
-    return f'<User {self.name=}, {self.email=}>'
+    return f'<User {self.name=}, {self.email=}, {self.followed_podcasts=}>'
 
-class Follow(db.Model, SerializerMixin):
+class Podcast(db.Model, SerializerMixin):
 
-  __tablename__ = 'follows'
-  serialize_rules = ('-user.ingredients', '-meals.ingredients', '-user.meals')
+  __tablename__ = 'podcasts'
+  # serialize_rules = ('-user.ingredients', '-meals.ingredients', '-user.meals')
 
   id = db.Column(db.Integer, primary_key = True)
-  itunes_id = db.Column(db.Integer, nullable=False)
-  user = db.relationship('User', back_populates='follows')
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  title = db.Column(db.String, nullable=False)
+  
 
   def __repr__(self):
-    return f'<Follow {self.user_id=}, {self.itunes_id=}>'
+    return f'<Podcast {self.title=}>'
+
+
