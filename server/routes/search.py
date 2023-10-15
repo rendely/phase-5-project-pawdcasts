4c7f1431d88039e2894eb061aae5758a7f0ac86b
matthew rendely <matthew.rendely@gmail.com>
2023-10-15 16:42:26 -0700

    increase quality of artwork in search api

--- server/routes/search.py
+++ server/routes/search.py
@@ -2,7 +2,7 @@ import requests
 from flask import request, session
 from flask_restful import Resource
 from config import api
-from models import User, Podcast
+from models import User, Podcast, follows
 import json
 
 class Search(Resource):
@@ -19,14 +19,12 @@ class Search(Resource):
         data = json.loads(r.text), 200
         results = [{"id": r.get('collectionId'), 
                     "name": r.get('collectionName'),
-                    "image": r.get('artworkUrl100'),
+                    "image": r.get('artworkUrl600'),
                     "itunes_url": r.get('collectionViewUrl'),
                     "genre": r.get('primaryGenreName'),
                     "track_count": r.get('trackCount')
                     } 
                     for r in data[0]['results']]
         return results, 200
-        # return query, 200
-        return [{ 'id': 1, 'name': 'Hiii' }, { 'id': 2, 'name': 'yooo' }], 200
 
 api.add_resource(Search, '/api/search')
