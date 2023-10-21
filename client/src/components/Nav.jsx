329f28ae53ed2bcef739b86cc38c455877f20323
matthew rendely <matthew.rendely@gmail.com>
2023-10-21 08:16:29 -0700

    Add spacer to unhide content blocked by nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,10 +6,14 @@ import pawdLogo from '/pawdcast_logo_large.png'
 export function Nav() {
   useEmoji()
 
-  return (<div className='nav-bar'>
+  return (
+  <>
+  <div className='spacer'></div>
+  <div className='nav-bar'>
     <NavLink className='nav-link' to='/'>Feed</NavLink>
     <NavLink className='nav-link' to='/search'>Search</NavLink>
     <NavLink className='nav-link' to='/me'>My Pawds</NavLink>
     <img src={pawdLogo} className="logo" alt="Pawdcasts logo" />
-  </div>)
+  </div>
+  </>)
 }
