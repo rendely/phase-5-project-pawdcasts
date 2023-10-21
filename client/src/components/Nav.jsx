c553d18ce9221473ebde75619371728d4c5da208
matthew rendely <matthew.rendely@gmail.com>
2023-10-20 18:24:28 -0700

    Add logo to navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,15 @@
 import './Nav.css'
 import { NavLink } from 'react-router-dom'
+import useEmoji from './useEmoji';
+import pawdLogo from '/pawdcast_logo_large.png'
 
 export function Nav() {
+  useEmoji()
 
   return (<div className='nav-bar'>
     <NavLink className='nav-link' to='/'>Feed</NavLink>
     <NavLink className='nav-link' to='/search'>Search</NavLink>
     <NavLink className='nav-link' to='/me'>My Pawds</NavLink>
+    <img src={pawdLogo} className="logo" alt="Pawdcasts logo" />
   </div>)
 }
