41536ae346e5244163804bf4f69b68e0ec108c2e
matthew rendely <matthew.rendely@gmail.com>
2023-10-20 17:42:51 -0700

    Move play/stop button to right side of episode card

--- client/src/components/Episode.jsx
+++ client/src/components/Episode.jsx
@@ -5,30 +5,34 @@ export default function Episode({ episode }) {
 
   const [isPlaying, setIsPlaying] = useState(false);
   const date = new Date(episode.date);
-  const formattedDate = date.toLocaleString().slice(0,9);
+  const formattedDate = date.toLocaleString().slice(0, 9);
 
   return (
     <div className='episode-result'>
-      <div className='episode-podcast-title'>
-        {episode.podcast_title}
-      </div>
-      <div className='episode-title'>
-        {episode.title}
-      </div>
-      <div className='episode-date'>
-        {formattedDate}
-      </div>
-      <div className='episode-audio'>
-        {isPlaying ?
-          <audio controls title={episode.title}>
-            <source src={episode.mp3} type="audio/mpeg" />
-          </audio>
-          :
-          null
-        }
+      <div className='episode-left'>
+
+
+        <div className='episode-podcast-title'>
+          {episode.podcast_title}
+        </div>
+        <div className='episode-title'>
+          {episode.title}
+        </div>
+        <div className='episode-date'>
+          {formattedDate}
+        </div>
+        <div className='episode-audio'>
+          {isPlaying ?
+            <audio controls autoPlay title={episode.title}>
+              <source src={episode.mp3} type="audio/mpeg" />
+            </audio>
+            :
+            null
+          }
+        </div>
       </div>
       <div className='episode-play-toggle'>
-        <button onClick={() => setIsPlaying(curr => !curr)}>{isPlaying ? 'Hide' : 'Play'}</button>
+        <button onClick={() => setIsPlaying(curr => !curr)}>{isPlaying ? 'Stop' : 'Play'}</button>
       </div>
     </div>
   )
