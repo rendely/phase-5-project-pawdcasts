fc031f3bee72c6a31d30e8bab549c7891d81d186
matthew rendely <matthew.rendely@gmail.com>
2024-01-20 22:02:34 -0800

    Add Player component, remove playing from episode list view since not used

--- client/src/components/Episode.jsx
+++ client/src/components/Episode.jsx
@@ -1,11 +1,8 @@
-import { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Episode.css'
 
 export default function Episode({ episode }) {
 
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audio = useRef();
 
   return (
     <div>
@@ -27,22 +24,11 @@ export default function Episode({ episode }) {
           </div>
 
         </div>
-        <div className='episode-play-toggle'>
-          <button onClick={() => setIsPlaying(curr => !curr)}>{isPlaying ? 'Stop' : 'Play'}</button>
-        </div>
+
 
       </div>
       <div className='episode-description'>
         {episode.description.slice(0, 200)}
-      </div>
-      <div className='episode-audio'>
-        {isPlaying ?
-          <audio ref={audio} controls autoPlay title={episode.title}>
-            <source src={episode.source_url} type="audio/mpeg" />
-          </audio>
-          :
-          null
-        }
       </div>
     </div>
   )
---
+++ client/src/components/Player.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from 'react';
+
+export default function Player({ currentEpisode }) {
+
+    const audio = useRef();
+
+    const timeoutIdRef = useRef(null);
+
+    useEffect(() => {
+        saveCurrentTime();
+        return () => {
+            if (timeoutIdRef.current) {
+                clearTimeout(timeoutIdRef.current);
+            }
+        }
+    }, [])
+
+    useEffect(() => {
+        if (audio.current) audio.current.currentTime = currentEpisode.current_time}
+    ,[currentEpisode]);
+
+    function saveCurrentTime() {
+        if (audio.current) {
+            const currentTime = Math.round(audio.current.currentTime);
+            fetch(`/api/episode/${currentEpisode.id}`, {
+                method: 'PATCH',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify({ current_time: currentTime })
+            });
+
+        }
+        timeoutIdRef.current = setTimeout(saveCurrentTime, 10000);
+    }
+
+
+    return (
+        <>
+            <audio
+                ref={audio}
+                controls autoPlay title={currentEpisode.title}>
+                <source src={currentEpisode.source_url} type="audio/mpeg" />
+            </audio>
+        </>)
+}
